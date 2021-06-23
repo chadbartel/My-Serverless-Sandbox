c1405651ae89847229f186e94024e72df94f49b4
chadbartel <bartel.chad@gmail.com>
2021-06-22 20:46:34 -0700

    move todos inside function, change logging level

--- lambda-ec2-tagging-monitor/handler.py
+++ lambda-ec2-tagging-monitor/handler.py
@@ -4,14 +4,16 @@ import logging
 
 # Setup logger
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-
-# TODO: List all EC2 instance(s)
-# TODO: Terminate EC2 instance(s) that don't meet tagging criteria
+logger.setLevel(logging.DEBUG)
 
 
 def run(event, context):
     current_time = datetime.datetime.now().time()
     name = context.function_name
     logger.info("Your cron function " + name + " ran at " + str(current_time))
+
+    # TODO: Load criteria
+
+    # TODO: Get instances w/invalid criteria
+ 
+    # TODO: Terminate instances w/invalid criteria
