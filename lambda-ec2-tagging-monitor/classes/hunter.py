bf1cfcd9afa80e114a898d6dfbfbebbbaeec80cb
chadbartel <bartel.chad@gmail.com>
2021-06-26 16:56:37 -0700

    create initial methods

--- lambda-ec2-tagging-monitor/classes/hunter.py
+++ lambda-ec2-tagging-monitor/classes/hunter.py
@@ -5,6 +5,8 @@
 import re
 import logging
 from copy import copy
+from classes.criteria import Criteria
+from classes.ec2client import EC2Client
 
 # Setup logger
 logger = logging.getLogger(__name__)
@@ -13,8 +15,19 @@ logger.setLevel(logging.DEBUG)
 
 class Hunter:
 
+    def __init__(self, criteria:Criteria, **client_kwargs):
+        self._client = EC2Client.get_client(**client_kwargs)
+
     # TODO: Get list of all active EC2 instances
+    def find_all_instances(self):
+        pass
+    
     # TODO: Identify any active EC2 instances with tags that violate criteria
+    def violating_instances(self):
+        pass
+    
     # TODO: Terminate all EC2 instances with invalid tagging
+    def terminate_instances(self):
+        pass
     
     pass
