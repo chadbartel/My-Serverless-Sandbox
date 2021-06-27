8e41dfca04e6a10c1418c623107314376ac08caf
chadbartel <bartel.chad@gmail.com>
2021-06-26 17:10:24 -0700

    rearrange class innards, update import ref

--- lambda-ec2-tagging-monitor/classes/hunter.py
+++ lambda-ec2-tagging-monitor/classes/hunter.py
@@ -7,7 +7,7 @@ import boto3
 import logging
 from copy import copy
 from classes.criteria import Criteria
-from classes.ec2client import EC2Client
+from classes.ec2 import EC2
 
 # Setup logger
 logger = logging.getLogger(__name__)
@@ -17,24 +17,6 @@ logger.setLevel(logging.DEBUG)
 class Hunter:
 
     def __init__(self, criteria:Criteria, **client_kwargs):
-        self._client = EC2Client.get_client(**client_kwargs)
-
-    # TODO: Get list of all active EC2 instances
-    def find_all_instances(self):
-        pass
-    
-    # TODO: Identify any active EC2 instances with tags that violate criteria
-    def violating_instances(self):
-        pass
-    
-    # TODO: Terminate all EC2 instances with invalid tagging
-
-    @property
-    def region(self):
-        return copy(self._region)
-    
-    def __init__(self, region:str='us-west-2'):
-        self._region = region
         self._base_filters = [
             {
                 'Name': 'instance-state-name',
@@ -45,12 +27,14 @@ class Hunter:
                 'Values': ['instance']
             }
         ]
-        pass
+        self._client = EC2.get_client(**client_kwargs)
 
-
-    def get_instances(self):
+    # TODO: Get list of all active EC2 instances
+    def find_all_instances(self):
         pass
     
-    def get_resource(self):
-        ec2 = boto3.resource('ec2', region_name=self._region)
-        return ec2
+    # TODO: Identify any active EC2 instances with tags that violate criteria
+    def violating_instances(self):
+        pass
+    
+    # TODO: Terminate all EC2 instances with invalid tagging
