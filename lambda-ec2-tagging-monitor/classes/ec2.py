4e09404468dc6c70fefe2b26440ab19c280c6189
chadbartel <bartel.chad@gmail.com>
2021-07-03 11:12:38 -0700

    remove resource method, flesh out init

--- lambda-ec2-tagging-monitor/classes/ec2.py
+++ lambda-ec2-tagging-monitor/classes/ec2.py
@@ -13,17 +13,30 @@ logger.setLevel(logging.DEBUG)
 class EC2:
 
     @classmethod
-    def get_client(cls, *args, **kwargs):
+    def get_session(cls, *args, **kwargs):
         args = [] if not args else args
         kwargs = {} if not kwargs else kwargs
         if 'region' not in kwargs:
             kwargs['region'] = 'us-west-2'
-        return boto3.client('ec2', *args, **kwargs)
+        return boto3.session(*args, **kwargs)
 
     @classmethod
-    def get_resource(cls, *args, **kwargs):
+    def get_client(cls, session:boto3.Session=None, *args, **kwargs):
         args = [] if not args else args
         kwargs = {} if not kwargs else kwargs
-        if 'region' not in kwargs:
-            kwargs['region'] = 'us-west-2'
-        return boto3.resource('ec2', *args, **kwargs)
+        session = cls.get_session() if not session else session
+        return session.client('ec2', *args, **kwargs)
+    
+    def __init__(self, region_name:str=None, profile_name:str=None, *args, **kwargs):
+        self._region_name = 'us-west-2' if not region_name else region_name
+        self._profile_name = profile_name
+        args = [] if not args else args
+        kwargs = {} if not kwargs else kwargs
+        self._session = self.get_session(
+            region_name=self._region_name,
+            profile_name=self._profile_name
+        )
+        self._client = self.get_client(self._session, 'ec2', *args, **kwargs)
+    
+    def list_instances(self, filters:dict=None):
+        pass
