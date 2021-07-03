2acc477c1adf24733c2dd44417ea7dbb85478774
chadbartel <bartel.chad@gmail.com>
2021-07-03 11:06:12 -0700

    remove erroneous code, change import behavior

--- lambda-ec2-tagging-monitor/classes/hunter.py
+++ lambda-ec2-tagging-monitor/classes/hunter.py
@@ -6,8 +6,8 @@ import re
 import boto3
 import logging
 from copy import copy
-from classes.criteria import Criteria
-from classes.ec2 import EC2
+from classes import Criteria
+from classes import EC2
 
 # Setup logger
 logger = logging.getLogger(__name__)
@@ -15,27 +15,14 @@ logger.setLevel(logging.DEBUG)
 
 
 class Hunter:
-
-    def _get_client(self, *args, **kwargs):
-        self._ec2_client = EC2.get_client(*args, **kwargs)
-
-    def __init__(self, criteria:Criteria, *client_args, **client_kwargs):
-        self._base_filters = [
-            {
-                'Name': 'instance-state-name',
-                'Values': ['running']
-            },
-            {
-                'Name': 'resource-type',
-                'Values': ['instance']
-            }
-        ]
-        self._client = self._get_client(*client_args, **client_kwargs)
+    
+    def __init__(self):
+        pass
 
     # TODO: Get list of all active EC2 instances
     def find_all_instances(self):
         self._instances = []
-    
+        return None
     
     # TODO: Identify any active EC2 instances with tags that violate criteria
     def violating_instances(self):
