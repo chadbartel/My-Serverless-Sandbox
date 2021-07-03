dbc1902308e283a1c11858eb599e0fceab3e1268
chadbartel <bartel.chad@gmail.com>
2021-07-03 15:25:19 -0700

    test EC2 class

--- lambda-ec2-tagging-monitor/tests/test.py
+++ lambda-ec2-tagging-monitor/tests/test.py
@@ -24,36 +24,11 @@ TAG_FILTERS = [
         'Values': ['instance']
     }
 ]
-INSTANCE_FILTERS = [
-    {
-        'Name': 'instance-state-name',
-        'Values': ['running']
-    }
-]
 
+ec2 = EC2(profile_name=AWS_PROFILE)
 
-def get_client(region_name='us-west-2', profile_name=None, *args, **kwargs):
-    session = boto3.Session(
-        region_name=region_name, 
-        profile_name=profile_name
-    )
-    args = [] if not args else args
-    kwargs = {} if not kwargs else kwargs
-    return session.client('ec2', *args, **kwargs)
-
-
-ec2_client = get_client(profile_name=AWS_PROFILE)
-instance_response = ec2_client.describe_instances(Filters=INSTANCE_FILTERS)
-try:
-    running_instances = [
-        i['InstanceId'] for i in instance_response['Reservations'][0]['Instances']
-    ]
-except IndexError as e:
-    running_instances = []
-    print(
-        f'No running instances found: {e}'
-    )
-tag_response = ec2_client.describe_tags(Filters=TAG_FILTERS)
+running_instances = ec2.list_instances()
+tag_response = ec2._client.describe_tags(Filters=TAG_FILTERS)
 instance_tags = [
     tag for tag in tag_response['Tags'] if tag['ResourceId'] in running_instances
 ]
