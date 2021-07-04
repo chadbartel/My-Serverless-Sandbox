dd797606d2dd8a56a9c70dcb76f2562b9869bd73
chadbartel <bartel.chad@gmail.com>
2021-07-03 21:45:17 -0700

    handle empty instances, loop over instances

--- lambda-ec2-tagging-monitor/handler.py
+++ lambda-ec2-tagging-monitor/handler.py
@@ -18,15 +18,30 @@ def run(event, context):
     current_time = datetime.datetime.now().time()
     name = context.function_name
     logger.info("Your cron function " + name + " ran at " + str(current_time))
-
-    # TODO: Load criteria
-    criteria = Criteria()
     
-    # TODO: Get EC2 class instance
+    # Get list of EC2 instances
     ec2 = EC2()
+    instances = ec2.list_instances()
+    if not instances:
+        logger.error(
+            'No running instances found'
+        )
+        sys.exit()
+
+    # Load instance tag criteria
+    criteria = Criteria()
 
     # TODO: Get instances w/invalid criteria
     hunter = Hunter(criteria)
- 
-    # TODO: Terminate instances w/invalid criteria
-    hunter.terminate_invalid_instances()
+
+    # Loop over list of instances
+    for i in instances:
+        # Get list of instance tags
+        tags = ec2.list_instance_tags()
+        
+        # Terminate instances without any tags
+        if not tags:
+            hunter.terminate_invalid_instances()
+        
+        # TODO: Terminate instances w/invalid criteria
+        hunter.terminate_invalid_instances()
