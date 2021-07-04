ae8ac240f77108a6d3628188ae68a044ad078c3f
chadbartel <bartel.chad@gmail.com>
2021-07-03 21:47:15 -0700

    remove redundant methods

--- lambda-ec2-tagging-monitor/classes/hunter.py
+++ lambda-ec2-tagging-monitor/classes/hunter.py
@@ -17,14 +17,9 @@ class Hunter:
     
     def __init__(self, criteria:Criteria):
         self.criteria = criteria
-
-    # TODO: Get list of all active EC2 instances
-    def find_all_instances(self):
-        self._instances = []
-        return None
     
-    # TODO: Identify any active EC2 instances with tags that violate criteria
+    # TODO: Identify instance tags that violate criteria
     def violating_instances(self):
         pass
     
-    # TODO: Terminate all EC2 instances with invalid tagging
+    # TODO: Terminate instance
